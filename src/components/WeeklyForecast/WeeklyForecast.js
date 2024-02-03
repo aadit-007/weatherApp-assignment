9e54b27dbec875effdf8625fbd243bcd34172a31
aadit-007 <adityanitk2024@gmail.com>
2024-02-03 20:40:16 +0530

    deployed warning fixed

--- src/components/WeeklyForecast/WeeklyForecast.js
+++ src/components/WeeklyForecast/WeeklyForecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Grid } from '@mui/material';
 import { getWeekDays } from '../../utilities/DatetimeUtils';
 import { weatherIcon } from '../../utilities/IconsUtils';
